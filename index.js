780390330568c374222296f12ed9e1bf2a0ee1ea
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2023-01-23 10:28:42 +0530

    made changes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from "express";
 import _routers from "./services/exports";
 import helmet from "helmet";
 import cors from "cors";
+import socket from "socket.io";
+import http from "http";
 
 import connectDB from "./db/connection";
 const __PORT__ = 5000;
@@ -13,8 +15,12 @@ app.use(helmet());
 
 app.use(express.json());
 
+const server = http.Server(app);
+const io = socket(server);
+
 app.use("/auth", _routers.auth);
 app.use("/rooms", _routers.chatroom);
+
 app.get("/", async (req, res) => {
   try {
     return res.status(200).json({ message: "Server Running Successfully" });
@@ -25,7 +31,11 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.listen(__PORT__, () => {
+io.on("connection", function (socket) {
+  console.log("Made socket connection");
+});
+
+server.listen(__PORT__, () => {
   connectDB()
     .then(() => {
       console.log(`Now listening on port ${__PORT__}\nDatabase Connected`);
